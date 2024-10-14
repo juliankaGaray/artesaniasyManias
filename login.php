f6a783a8514069f832e4d47abcf8d03a1c0cb7a7
jalz <jalzusaquen@gmail.com>
2024-10-14 16:10:01 -0500

    style2login

--- login.php
+++ login.php
@@ -1,8 +1,8 @@
-
-
 <?php
 session_start();
 include('controllers/connection.php'); // Incluir el archivo connection.php
+
+$error = ""; // Variable para manejar errores
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $usuario = $_POST['usuario'];
@@ -37,68 +37,87 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             } elseif ($user['rol_nombre'] == 'viewer') {
                 header("Location: viewer_dashboard.php"); // Página solo para vistas
             } else {
-                // Si el rol no está mapeado, redirigir a una página de error o logout
                 header("Location: logout.php");
             }
             exit();
         } else {
             // Si las credenciales no son válidas
-            echo "Usuario o contraseña incorrectos.";
+            $error = "Usuario o contraseña incorrectos.";
         }
     } else {
-        echo "Error al preparar la consulta: " . $conn->error;
+        $error = "Error al preparar la consulta: " . $conn->error;
     }
 
     // Cerrar la conexión
     $conn->close();
 }
 ?>
-<?php include('header.php') ?>
 
-<!-- Mostrar mensajes -->
-<div class="container mt-3">
-<?php if (!empty($error)): ?>
-    <div class="alert alert-danger"><?php echo $error; ?></div>
-<?php endif; ?>
-</div>
+<?php include('header.php'); ?>
 
-
-
-<!-- formulario de login-->
-<form class="container-fluid" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-    <section class="py-5">
-        <div class="container px-5">
-            <div class="bg-light rounded-4 py-5 px-4 px-md-5">
-                <div class="text-center mb-5">
-                    <div class="feature text-white rounded-3 mb-3">
-                        <img src="docs/assets/images/icons/clipboard.svg" alt="Icono personalizado" class="svg-icon">
+<div class="page-header align-items-start min-vh-100">
+        
+    <div class="container my-auto">
+        <div class="row">
+            <div class="col-lg-4 col-md-8 col-12 mx-auto">
+                <div class="card z-index-0 fadeIn3 fadeInBottom">
+                    <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
+                        <div class="bg-gradient-primary shadow-primary border-radius-lg py-3 pe-1">
+                            <h4 class="text-white font-weight-bolder text-center mt-2 mb-0">Iniciar Sesion</h4>
+                        </div>
                     </div>
-                    <h1 class="fw-bolder">Inicia sesión</h1>
-                    <p class="lead fw-normal text-muted mb-0">Introduce tus credenciales</p>
-                </div>
-                <div class="row gx-5 justify-content-center">
-                    <div class="col-lg-8 col-xl-6">
-                        <div class="form-floating mb-3">
-                            <input type="text" name="usuario" class="form-control" id="usuario" placeholder="miusuario00" required>
-                            <label for="usuario">Usuario</label>
-                        </div>
-                        <div class="form-floating mb-3">
-                            <input type="password" name="password" class="form-control" id="password" placeholder="Contraseña" required>
-                            <label for="password">Contraseña</label>
-                        </div>
-                        <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
-                            <a class="small" href="recuperarcontraseña.php">olvido la contraseña</a>
-                        </div>
-                        <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
-                            <a class="small" href="registro.php">registrate</a>
-                        </div>
-                        <div class="d-grid">
-                            <button class="btn btn-primary btn-lg" type="submit">Enviar</button>
-                        </div>
+                    <div class="card-body">
+                        <!-- Mostrar mensajes de error -->
+                        <?php if (!empty($error)): ?>
+                            <div class="alert alert-danger"><?php echo $error; ?></div>
+                        <?php endif; ?>
+                        <form role="form" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" class="text-start">
+                            <div class="input-group input-group-outline my-3">
+                                <label class="form-label">Usuario</label>
+                                <input type="text" name="usuario" class="form-control" required>
+                            </div>
+                            <div class="input-group input-group-outline mb-3">
+                                <label class="form-label">Contraseña</label>
+                                <input type="password" name="password" class="form-control" required>
+                            </div>
+                            <div class="form-check form-switch d-flex align-items-center mb-3">
+                                <input class="form-check-input" type="checkbox" id="rememberMe">
+                                <label class="form-check-label mb-0 ms-3" for="rememberMe">Recordarme</label>
+                            </div>
+                            <div class="text-center">
+                                <button type="submit" class="btn bg-gradient-primary w-100 my-4 mb-2">Iniciar sesión</button>
+                            </div>
+                            <p class="mt-4 text-sm text-center">
+                                ¿No tienes una cuenta? <a href="registro.php">Regístrate</a>
+                            </p>
+                        </form>
                     </div>
                 </div>
             </div>
         </div>
-    </section>
+    </div>
+    <footer class="footer position-absolute bottom-2 py-2 w-100">
+        <div class="container">
+            <div class="row align-items-center justify-content-lg-between">
+                <div class="col-12 col-md-6 my-auto">
+                    <div class="copyright text-center text-sm text-white text-lg-start">
+                        © <script>document.write(new Date().getFullYear())</script>,
+                        hecho con <i class="fa fa-heart" aria-hidden="true"></i> por
+                        <a href="https://www.creative-tim.com" class="font-weight-bold text-white" target="_blank">Creative Tim</a>
+                        para una mejor web.
+                    </div>
+                </div>
+                <div class="col-12 col-md-6">
+                    <ul class="nav nav-footer justify-content-center justify-content-lg-end">
+                        <li class="nav-item"><a href="https://www.creative-tim.com" class="nav-link text-white" target="_blank">Creative Tim</a></li>
+                        <li class="nav-item"><a href="https://www.creative-tim.com/presentation" class="nav-link text-white" target="_blank">Sobre nosotros</a></li>
+                        <li class="nav-item"><a href="https://www.creative-tim.com/blog" class="nav-link text-white" target="_blank">Blog</a></li>
+                        <li class="nav-item"><a href="https://www.creative-tim.com/license" class="nav-link pe-0 text-white" target="_blank">Licencia</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+</div>
 
-    <?php include('footer.php') ?>
+<?php include('footer.php'); ?>
