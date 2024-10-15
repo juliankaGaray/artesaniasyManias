3dd1d9b00b9489e1cbe6ffc4a5f00501848abd67
Julian Garay <julcamgar@gmail.com>
2024-10-15 12:31:10 -0500

    catalogo responsivo

--- catalogo.php
+++ catalogo.php
@@ -6,43 +6,36 @@ $sentencia = $base_de_datos->query("SELECT * FROM productos;");
 $productos = $sentencia->fetchAll(PDO::FETCH_OBJ);
 ?>
 
-<div class="container-fluid bg-white p-4" style="border-radius: 8px; box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);">
+<div class="container bg-white p-4" style="max-width: 1200px; border-radius: 8px; box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);">
   <div class="col-xs-12">
-    <h1>Catalogo de Productos</h1>
+    <h1 class="text-center">Catálogo de Productos</h1>
     
     <br>
     <div class="table-responsive">
-      <table class="table table-bordered" style="width: 100%;">
+      <table class="table table-striped table-hover" style="width: 100%;">
         <thead>
           <tr>
-            
+            <th>Imagen</th>
             <th>Productos</th>
-            
-            <th>Precio </th>
-            
-            <th>Imagen</th> <!-- Nueva columna para la imagen -->
-            <th>Realiza tu compra</th> <!-- Nueva columna para acciones -->
+            <th>Precio</th>
+            <th>Realiza tu compra</th>
           </tr>
         </thead>
         <tbody>
           <?php foreach($productos as $producto){ ?>
           <tr>
-            
-            <td><?php echo $producto->descripcion ?></td>
-            
-            <td><?php echo $producto->precioVenta ?></td>
-            
             <td>
-                <img src="ventas/uploads/<?php echo str_replace('uploads/', '', $producto->imagen) ?>" alt="<?php echo $producto->descripcion ?>" style="width: 50px; height: auto;">
+                <img src="ventas/uploads/<?php echo str_replace('uploads/', '', $producto->imagen) ?>" 
+                     alt="<?php echo $producto->descripcion ?>" 
+                     style="width: 100%; max-width: 150px; height: auto; border: 2px solid #007bff; border-radius: 5px; box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.2);">
             </td>
+            <td><?php echo $producto->descripcion ?></td>
+            <td><?php echo $producto->precioVenta ?></td>
             <td>
-              <!-- Botón para agregar a la venta -->
-              <a class="btn btn-info" href="/compras/vender.php?id=<?php echo $producto->id; ?>">
+              <a class="btn btn-success" href="/compras/vender.php?id=<?php echo $producto->id; ?>">
                   <i class="fas fa-shopping-cart"></i> Comprar
               </a>
-              
             </td>
-
           </tr>
           <?php } ?>
         </tbody>
--- compras/ventas.php
+++ compras/ventas.php
@@ -1,60 +1,67 @@
-<?php include_once "encabezado.php" ?>
+<?php include_once "encabezado.php"; ?>
 <?php
 include_once "base_de_datos.php";
-$sentencia = $base_de_datos->query("SELECT ventas.total, ventas.fecha, ventas.id, GROUP_CONCAT(	productos.codigo, '..',  productos.descripcion, '..', productos_vendidos.cantidad SEPARATOR '__') AS productos FROM ventas INNER JOIN productos_vendidos ON productos_vendidos.id_venta = ventas.id INNER JOIN productos ON productos.id = productos_vendidos.id_producto GROUP BY ventas.id ORDER BY ventas.id;");
+$sentencia = $base_de_datos->query("SELECT ventas.total, ventas.fecha, ventas.id, GROUP_CONCAT(productos.codigo, '..', productos.descripcion, '..', productos_vendidos.cantidad SEPARATOR '__') AS productos FROM ventas INNER JOIN productos_vendidos ON productos_vendidos.id_venta = ventas.id INNER JOIN productos ON productos.id = productos_vendidos.id_producto GROUP BY ventas.id ORDER BY ventas.id;");
 $ventas = $sentencia->fetchAll(PDO::FETCH_OBJ);
 ?>
 
-	<div class="col-xs-12">
-		<h1>Ventas</h1>
-		<div>
-			<a class="btn btn-success" href="./vender.php">Nueva <i class="fa fa-plus"></i></a>
-		</div>
-		<br>
-		<table class="table table-bordered">
-			<thead>
-				<tr>
-					<th>Número</th>
-					<th>Fecha</th>
-					<th>Productos vendidos</th>
-					<th>Total</th>
-					<th>Ticket</th>
-					<th>Eliminar</th>
-				</tr>
-			</thead>
-			<tbody>
-				<?php foreach($ventas as $venta){ ?>
-				<tr>
-					<td><?php echo $venta->id ?></td>
-					<td><?php echo $venta->fecha ?></td>
-					<td>
-						<table class="table table-bordered">
-							<thead>
-								<tr>
-									<th>Código</th>
-									<th>Descripción</th>
-									<th>Cantidad</th>
-								</tr>
-							</thead>
-							<tbody>
-								<?php foreach(explode("__", $venta->productos) as $productosConcatenados){ 
-								$producto = explode("..", $productosConcatenados)
-								?>
-								<tr>
-									<td><?php echo $producto[0] ?></td>
-									<td><?php echo $producto[1] ?></td>
-									<td><?php echo $producto[2] ?></td>
-								</tr>
-								<?php } ?>
-							</tbody>
-						</table>
-					</td>
-					<td><?php echo $venta->total ?></td>
-					<td><a class="btn btn-info" href="<?php echo "imprimirTicket.php?id=" . $venta->id?>"><i class="fa fa-print"></i></a></td>
-					<td><a class="btn btn-danger" href="<?php echo "eliminarVenta.php?id=" . $venta->id?>"><i class="fa fa-trash"></i></a></td>
-				</tr>
-				<?php } ?>
-			</tbody>
-		</table>
-	</div>
-<?php include_once "pie.php" ?>
+<div class="container bg-white p-4" style="max-width: 1200px; border-radius: 8px; box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);">
+    <div class="col-xs-12">
+        <h1 class="text-center">Ventas</h1>
+        <div class="text-right">
+            <a class="btn btn-success" href="./vender.php">Nueva <i class="fa fa-plus"></i></a>
+        </div>
+        <br>
+        <div class="table-responsive">
+            <table class="table table-bordered table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>Número</th>
+                        <th>Fecha</th>
+                        <th>Productos vendidos</th>
+                        <th>Total</th>
+                        <th>Ticket</th>
+                        <th>Eliminar</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach($ventas as $venta){ ?>
+                    <tr>
+                        <td><?php echo $venta->id ?></td>
+                        <td><?php echo $venta->fecha ?></td>
+                        <td>
+                            <div class="table-responsive">
+                                <table class="table table-bordered">
+                                    <thead>
+                                        <tr>
+                                            <th>Código</th>
+                                            <th>Descripción</th>
+                                            <th>Cantidad</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php foreach(explode("__", $venta->productos) as $productosConcatenados){ 
+                                        $producto = explode("..", $productosConcatenados);
+                                        ?>
+                                        <tr>
+                                            <td><?php echo $producto[0] ?></td>
+                                            <td><?php echo $producto[1] ?></td>
+                                            <td><?php echo $producto[2] ?></td>
+                                        </tr>
+                                        <?php } ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </td>
+                        <td><?php echo $venta->total ?></td>
+                        <td><a class="btn btn-info" href="<?php echo "imprimirTicket.php?id=" . $venta->id; ?>"><i class="fa fa-print"></i></a></td>
+                        <td><a class="btn btn-danger" href="<?php echo "eliminarVenta.php?id=" . $venta->id; ?>"><i class="fa fa-trash"></i></a></td>
+                    </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+<?php include_once "pie.php"; ?>
